af28aaae56dda5fc70c05906995c0c3b82b76727
TangNi <wyntny@163.com>
2017-02-13 14:02:43 +0800

    axios读取本地json数据文件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <template>
   <div>
-  <p>{{ total }}</p>
-  <button-counter v-on:increment="incrementTotal"></button-counter>
-  <button-counter v-on:increment="incrementTotal"></button-counter>
-</div>
+    <headernav></headernav>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="Name" label="名称" width="180">
+      </el-table-column>
+      <el-table-column prop="Url" label="地址" width="180">
+      </el-table-column>
+      <el-table-column prop="Country" label="国家">
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
-
 <script>
-import buttonCounter from './components/buttonCounter/index.vue'
-export default{
-components:{
-buttonCounter
-},
+  import headernav from "components/Headernav.vue"
+  export default {
+    components: {
+      headernav
+    },
+    data() {
+      return {
+        tableData: []
+      }
+    },
+    created() {
+    var vm=this
+    this.$http.get('../static/json/data.json').then(function(response) {
+      vm.tableData = response.data.tableData
+        }) .catch(function(response) {
+          console.log(response)
+        })
+    }
 
-  data () {
-    return{
-    total: 0
-    }
-  },
-  methods: {
-    incrementTotal: function () {
-      this.total += 1
-    }
   }
-}
 </script>
